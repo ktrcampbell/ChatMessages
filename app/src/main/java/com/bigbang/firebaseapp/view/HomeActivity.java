32e42cf812c25b9a24456ed0fa361505b15aff53
Katrina Campbell <katrina.campbell@enhanceit.us>
2020-03-28 17:19:59 -0400

    MVVM refactoring

--- app/src/main/java/com/bigbang/firebaseapp/view/HomeActivity.java
+++ app/src/main/java/com/bigbang/firebaseapp/view/HomeActivity.java
@@ -2,78 +2,88 @@ package com.bigbang.firebaseapp.view;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.bigbang.firebaseapp.R;
+import com.bigbang.firebaseapp.adapter.ChatMessageAdapter;
 import com.bigbang.firebaseapp.model.ChatMessage;
+import com.bigbang.firebaseapp.viewmodel.ChatViewModel;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.core.view.View;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
-import io.reactivex.Observable;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class HomeActivity extends AppCompatActivity {
 
-    //instatiate db reference
-    DatabaseReference reference;
-    private List<ChatMessage> chatMessages = new ArrayList<>();
+    private String username = "Katrina";
+    private Observer<List<ChatMessage>> messageObservable;
 
+    private ChatViewModel viewModel;
+
+    @BindView(R.id.chat_message_receyclerview)
+    RecyclerView chatRecyclerView;
+
+    @BindView(R.id.message_edittext)
+    EditText messageEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        //create database instance to insert new message
-        reference = FirebaseDatabase.getInstance().getReference().child("messages/");
+        viewModel = ViewModelProviders.of(this).get(ChatViewModel.class);
 
-        ChatMessage sendMessage = new ChatMessage("Katrina",
-                "Is this working?", "03/26/2020");
-
-        //push new message to db
-        String pushValue = reference.push().getKey();
-        if (pushValue != null) {
-
-            Log.d("TAG_K", "Success : " + pushValue);
-            reference.child(pushValue).setValue(sendMessage);
-
-        } else {
-            Log.d("TAG_K", "failed to push value");
-        }
-
-        //listen for changes and update db
-        reference.addValueEventListener(new ValueEventListener() {
+        messageObservable = new Observer<List<ChatMessage>>() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
-                for (DataSnapshot dSnapshot : dataSnapshot.getChildren()) {
-                    //This is where recycler view steps in in all its glory
-                    ChatMessage message = dSnapshot.getValue(ChatMessage.class);
-                    chatMessages.add(message);
-                }
-
-                printChildMessages(chatMessages);
-
+            public void onChanged(List<ChatMessage> chatMessages) {
+                displayMessages(chatMessages);
             }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
+        };
+        viewModel.getChatMessages().observe(this, messageObservable);
     }
 
-    private void printChildMessages(List<ChatMessage> chatMessages) {
-        for (int i = 0; i < chatMessages.size() ; i++) {
-            Log.d("TAG_K", chatMessages.get(i).toString());
+    private void displayMessages(List<ChatMessage> chatMessages) {
+        ChatMessageAdapter chatMessage = new ChatMessageAdapter(chatMessages);
+        chatRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        chatRecyclerView.setAdapter(chatMessage);
+
+    }
+
+    @OnClick(R.id.send_button)
+    public void sendMessage(View view){
+        String message = "";
+        if(!messageEditText.getText().toString().trim().isEmpty()) {
+            message = messageEditText.getText().toString().trim();
+            messageEditText.setText("");
+            ChatMessage chatMessage = new ChatMessage(username, new SimpleDateFormat("MM/dd/yyyy",
+                    Locale.US).format(new Date()), message);
+            viewModel.sendNewMessage(chatMessage);
+            Toast.makeText(this, "Message sent.", Toast.LENGTH_SHORT).show();
         }
+
     }
 }
+
+
 
